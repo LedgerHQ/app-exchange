4c52cc19ef356674252b16036968b15888ac11d5
François Colas <francois.colas@ledger.fr>
2020-07-16 11:13:24 +0200

    Add SET_PARTNER_KEY command for SELL

--- src/check_partner.c
+++ src/check_partner.c
@@ -14,18 +14,26 @@ int check_partner(subcommand_e subcommand,                                      
     if (input_buffer_length < MIN_DER_SIGNATURE_LENGTH ||
         input_buffer_length > MAX_DER_SIGNATURE_LENGTH) {
         PRINTF("Error: Input buffer length don't correspond to DER length\n");
+
         return reply_error(ctx, INCORRECT_COMMAND_DATA, send);
     }
+
     if (cx_ecdsa_verify(&(ctx->ledger_public_key), CX_LAST, CX_SHA256, ctx->sha256_digest,
                         CURVE_SIZE_BYTES, input_buffer, input_buffer_length) == 0) {
         PRINTF("Error: Failed to verify signature of partner data\n");
+
         return reply_error(ctx, SIGN_VERIFICATION_FAIL, send);
     }
+
     unsigned char output_buffer[2] = {0x90, 0x00};
+
     if (send(output_buffer, 2) < 0) {
         PRINTF("Error: send error\n");
+
         return -1;
     }
+
     ctx->state = PROVIDER_CHECKED;
+
     return 0;
 }
