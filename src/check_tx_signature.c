e50d7dbbd0e2130a1ea779e1e2b2c07994432d3f
greenknot <greenknot@users.noreply.github.com>
2020-09-30 17:17:29 +0200

    remove stack-based memory allocation in check_tx_signature

--- src/check_tx_signature.c
+++ src/check_tx_signature.c
@@ -5,6 +5,8 @@
 #include "reply_error.h"
 #include "der.h"
 
+#define MAX_DER_INT_SIZE(size) (1 + 3 + (size) + 1)
+
 // This function receive transaction signature
 // Input should be in the form of DER serialized signature
 // the length should be CURVE_SIZE_BYTES * 2 + 6 (DER encoding)
@@ -34,16 +36,22 @@ int check_tx_signature(subcommand_e subcommand,                                 
 
         size_t der_r_len = asn1_get_encoded_integer_size(input_buffer, 32);
         size_t der_s_len = asn1_get_encoded_integer_size(input_buffer + 32, 32);
+        size_t size = der_r_len + der_s_len + 2;
 
-        unsigned char der_sig[der_r_len + der_s_len + 2];
+        unsigned char der_sig[MAX_DER_INT_SIZE(32) * 2 + 2];
+        if (size > sizeof(der_sig)) {
+            PRINTF("Error: Unexpected der integer size");
+            return reply_error(ctx, SIGN_VERIFICATION_FAIL, send);
+        }
 
-        encode_sig_der(der_sig, der_r_len + der_s_len + 2, input_buffer, 32, input_buffer + 32, 32);
 
-        PRINTF("DER sig: %.*H\n", sizeof(der_sig), der_sig);
+        encode_sig_der(der_sig, size, input_buffer, 32, input_buffer + 32, 32);
+
+        PRINTF("DER sig: %.*H\n", size, der_sig);
         PRINTF("SHA256(payload): %.*H\n", sizeof(ctx->sha256_digest), ctx->sha256_digest);
 
         if (cx_ecdsa_verify(&ctx->partner.public_key, CX_LAST, CX_SHA256, ctx->sha256_digest,
-                            CURVE_SIZE_BYTES, der_sig, sizeof(der_sig)) == 0) {
+                            CURVE_SIZE_BYTES, der_sig, size) == 0) {
             PRINTF("Error: Failed to verify signature of received transaction");
             return reply_error(ctx, SIGN_VERIFICATION_FAIL, send);
         }
