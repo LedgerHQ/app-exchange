3d46d0534aad2a69e8cd33176773a4891c28ab50
pscott <scott.piriou@ledger.fr>
2021-02-11 16:04:47 +0100

    Fix unexpceted_command signature

--- src/unexpected_command.c
+++ src/unexpected_command.c
@@ -2,6 +2,10 @@
 #include "reply_error.h"
 #include "swap_errors.h"
 
-int unexpected_command(rate_e P1, swap_app_context_t *ctx, const buf_t *input, SendFunction send) {
+int unexpected_command(rate_e P1,
+                       subcommand_e P2,
+                       swap_app_context_t *ctx,
+                       const buf_t *input,
+                       SendFunction send) {
     return reply_error(ctx, INVALID_INSTRUCTION, send);
 }
--- src/unexpected_command.h
+++ src/unexpected_command.h
@@ -5,6 +5,10 @@
 #include "send_function.h"
 #include "commands.h"
 
-int unexpected_command(rate_e P1, swap_app_context_t *ctx, const buf_t *input, SendFunction send);
+int unexpected_command(rate_e P1,
+                       subcommand_e P2,
+                       swap_app_context_t *ctx,
+                       const buf_t *input,
+                       SendFunction send);
 
 #endif  //_UNEXPECTED_COMMAND_H_
