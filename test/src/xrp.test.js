5efe3542283f5af11235d5fd9f50a568ecbe42ca
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-10-19 17:17:17 +0200

    Update tests to better standards

--- test/src/xrp.test.js
+++ test/src/xrp.test.js
@@ -12,175 +12,133 @@ import {
 } from "./common";
 import Exchange from "./exchange.js";
 import {
-    TRANSACTION_RATES,
     TRANSACTION_TYPES
 } from "./exchange.js";
-import Zemu from "@zondax/zemu";
 import { TransportStatusError } from "@ledgerhq/errors";
 
-const sim_options = {
-    logging: true,
-    start_delay: 1500,
-    X11: true,
-    model: "nanos",
-};
-const Resolve = require("path").resolve;
-const APP_PATH = Resolve("elfs/exchange.elf");
-const XRP_LIB = { "XRP": Resolve("elfs/xrp.elf") };
-jest.setTimeout(50000);
+import { waitForAppScreen, zemu } from './test.fixture';
 
-test('Wrong payout address XRP should not be accepted', async () => {
-    const sim = new Zemu(APP_PATH, XRP_LIB);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("Ka7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAK");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("BTC");
-        tr.setCurrencyTo("XRP");
-        // 1 BTC to 10 XRP
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+test('[Nano S] Wrong XRP payout address should not be accepted', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("Ka7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAK");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("BTC");
+    tr.setCurrencyTo("XRP");
+    // 1 BTC to 10 XRP
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const params = await getSerializedAddressParameters("44'/144'/0'/0/0");
-        await expect(swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, params.addressParameters))
-            .rejects.toEqual(new TransportStatusError(0x6a83));
-    } finally {
-        await sim.close();
-    }
-})
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const params = await getSerializedAddressParameters("44'/144'/0'/0/0");
+    await expect(swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, params.addressParameters))
+        .rejects.toEqual(new TransportStatusError(0x6a83));
+}));
 
-test('Valid payout address XRP should be accepted', async () => {
-    const sim = new Zemu(APP_PATH, XRP_LIB);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("BTC");
-        tr.setCurrencyTo("XRP");
-        // 1 BTC to 10 XRP
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+test('[Nano S] Valid payout address XRP should be accepted', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("BTC");
+    tr.setCurrencyTo("XRP");
+    // 1 BTC to 10 XRP
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const params = await getSerializedAddressParameters("44'/144'/0'/0/0");
-        await expect(swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, params.addressParameters)).resolves.toBe(undefined);
-    } finally {
-        await sim.close();
-    }
-})
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const params = await getSerializedAddressParameters("44'/144'/0'/0/0");
+    await expect(swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, params.addressParameters)).resolves.toBe(undefined);
+}));
 
+test('[Nano S] Wrong refund address should be rejected', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz5");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("XRP");
+    tr.setCurrencyTo("XRP");
+    // 1 XRP to 10 XRP
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
+    await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
 
-test('Wrong refund address should be rejected', async () => {
-    const sim = new Zemu(APP_PATH, XRP_LIB);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz5");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("XRP");
-        tr.setCurrencyTo("XRP");
-        // 1 XRP to 10 XRP
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+    const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
+    await expect(swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters))
+        .rejects.toEqual(new TransportStatusError(0x6a83));
+}));
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
-        await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
+test('[Nano S] Valid refund address should be accepted', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz4");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("XRP");
+    tr.setCurrencyTo("XRP");
+    // 1 XRP to 10 XRP
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
-        await expect(swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters))
-            .rejects.toEqual(new TransportStatusError(0x6a83));
-    } finally {
-        await sim.close();
-    }
-})
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
+    await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
 
-test('Valid refund address should be accepted', async () => {
-    const sim = new Zemu(APP_PATH, XRP_LIB);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz4");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("XRP");
-        tr.setCurrencyTo("XRP");
-        // 1 XRP to 10 XRP
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+    const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
+    const checkRequest = swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
-        await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
+    await waitForAppScreen(sim);
+    await sim.navigateAndCompareSnapshots('.', 'nanos_xrp_valid_refund_address_is_accepted', [4, 0]);
 
-        const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
-        const checkRequest = swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters);
-        // Wait until we are not in the main menu
-        await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-
-        await sim.clickRight();
-        await sim.clickRight();
-        await sim.clickRight();
-        await sim.clickRight();
-        await sim.clickBoth();
-
-        await expect(checkRequest).resolves.toBe(undefined);
-    } finally {
-        await sim.close();
-    }
-})
+    await expect(checkRequest).resolves.toBe(undefined);
+}));
