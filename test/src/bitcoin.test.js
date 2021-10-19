10e84fee6f5611187a3a57d0551709e762fc4587
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-10-19 17:17:17 +0200

    Update tests to better standards

--- test/src/bitcoin.test.js
+++ test/src/bitcoin.test.js
@@ -14,178 +14,137 @@ import {
 } from "./common";
 import Exchange from "./exchange.js";
 import {
-    TRANSACTION_RATES,
     TRANSACTION_TYPES
 } from "./exchange.js";
 import Zemu from "@zondax/zemu";
 import { TransportStatusError } from "@ledgerhq/errors";
 
-const sim_options = {
-    logging: true,
-    start_delay: 1500,
-    X11: true,
-    model: "nanos",
-};
-const Resolve = require("path").resolve;
-const APP_PATH = Resolve("elfs/exchange.elf");
-const BTC_LIBS = { "Bitcoin": Resolve("elfs/bitcoin.elf"), "Litecoin": Resolve("elfs/litecoin.elf") };
-jest.setTimeout(50000);
+import { waitForAppScreen, zemu } from './test.fixture';
 
-test('Wrong payout address should be rejected', async () => {
-    const sim = new Zemu(APP_PATH, BTC_LIBS);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjL");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("BTC");
-        tr.setCurrencyTo("LTC");
-        // 1 BTC to 1 LTC
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(48430000000000000000));
-        tr.setDeviceTransactionId(transactionId);
+test('[Nano S] Wrong payout address should be rejected', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjL");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("BTC");
+    tr.setCurrencyTo("LTC");
+    // 1 BTC to 1 LTC
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(48430000000000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const params = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
-        console.log(params);
-        await expect(swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, params.addressParameters))
-            .rejects.toEqual(new TransportStatusError(0x6a83));
-    } finally {
-        await sim.close();
-    }
-})
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const params = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
+    console.log(params);
+    await expect(swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, params.addressParameters))
+        .rejects.toEqual(new TransportStatusError(0x6a83));
+}));
 
+test('[Nano S] Valid Bitcoin payout address should be accepted', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjD");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("BTC");
+    tr.setCurrencyTo("LTC");
+    // 1 BTC to 10 LTC
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-test('Valid payout address should be accepted', async () => {
-    const sim = new Zemu(APP_PATH, BTC_LIBS);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjD");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("BTC");
-        tr.setCurrencyTo("LTC");
-        // 1 BTC to 10 LTC
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const params = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
+    console.log(params);
+    await expect(swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, params.addressParameters)).resolves.toBe(undefined);
+}));
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const params = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
-        console.log(params);
-        await expect(swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, params.addressParameters)).resolves.toBe(undefined);
-    } finally {
-        await sim.close();
-    }
-})
+test('[Nano S] Wrong Bitcoin refund address should not be accepted', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjD");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("BTC");
+    tr.setCurrencyTo("LTC");
+    // 1 BTC to 10 LTC
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const ltcAddressParams = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
+    await swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, ltcAddressParams.addressParameters);
 
-test('Wrong refund address should be rejected', async () => {
-    const sim = new Zemu(APP_PATH, BTC_LIBS);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("sfdsfdsfsdfdsfsdf");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjD");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("BTC");
-        tr.setCurrencyTo("LTC");
-        // 1 BTC to 10 LTC
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+    const btcAddressParams = await getSerializedAddressParametersBTC("84'/0'/0'/1/0", "bech32");
+    await expect(swap.checkRefundAddress(BTCConfig, BTCConfigSignature, btcAddressParams.addressParameters))
+        .rejects.toEqual(new TransportStatusError(0x6a83));
+}));
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const ltcAddressParams = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
-        await swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, ltcAddressParams.addressParameters);
+test('[Nano S] Valid refund address should be accepted', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("bc1qwpgezdcy7g6khsald7cww42lva5g5dmasn6y2z");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjD");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("BTC");
+    tr.setCurrencyTo("LTC");
+    // 1 BTC to 10 LTC
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        const btcAddressParams = await getSerializedAddressParametersBTC("84'/0'/0'/1/0", "bech32");
-        await expect(swap.checkRefundAddress(BTCConfig, BTCConfigSignature, btcAddressParams.addressParameters))
-            .rejects.toEqual(new TransportStatusError(0x6a83));
-    } finally {
-        await sim.close();
-    }
-})
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const ltcAddressParams = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
+    await swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, ltcAddressParams.addressParameters);
 
+    const btcAddressParams = await getSerializedAddressParametersBTC("84'/0'/0'/1/0", "bech32");
+    const checkRequest = swap.checkRefundAddress(BTCConfig, BTCConfigSignature, btcAddressParams.addressParameters);
 
-test('Valid refund address should be accepted', async () => {
-    const sim = new Zemu(APP_PATH, BTC_LIBS);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("bc1qwpgezdcy7g6khsald7cww42lva5g5dmasn6y2z");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("LKtSt6xfsmJMkPT8YyViAsDeRh7k8UfNjD");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("BTC");
-        tr.setCurrencyTo("LTC");
-        // 1 BTC to 10 LTC
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+    // Wait until we are not in the main menu
+    await waitForAppScreen(sim);
+    await sim.navigateAndCompareSnapshots('.', 'nanos_btc_valid_refund_address_is_accepted', [4, 0]);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const ltcAddressParams = await getSerializedAddressParametersBTC("49'/0'/0'/0/0");
-        await swap.checkPayoutAddress(LTCConfig, LTCConfigSignature, ltcAddressParams.addressParameters);
-
-        const btcAddressParams = await getSerializedAddressParametersBTC("84'/0'/0'/1/0", "bech32");
-        const checkRequest = swap.checkRefundAddress(BTCConfig, BTCConfigSignature, btcAddressParams.addressParameters);
-        // Wait until we are not in the main menu
-        await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-
-        await sim.clickRight();
-        await sim.clickRight();
-        await sim.clickRight();
-        await sim.clickRight();
-        await sim.clickBoth();
-
-        await expect(checkRequest).resolves.toBe(undefined);
-    } finally {
-        await sim.close();
-    }
-})
+    await expect(checkRequest).resolves.toBe(undefined);
+}));
--- test/src/rates.test.js
+++ test/src/rates.test.js
@@ -15,144 +15,78 @@ import {
     TRANSACTION_RATES,
     TRANSACTION_TYPES
 } from "./exchange.js";
-import Zemu from "@zondax/zemu";
-import { TransportStatusError } from "@ledgerhq/errors";
 
-const sim_options = {
-    logging: true,
-    start_delay: 1500,
-    X11: true,
-    model: "nanos",
-};
-const Resolve = require("path").resolve;
-const APP_PATH = Resolve("elfs/exchange.elf");
-const XRP_LIB = { "XRP": Resolve("elfs/xrp.elf") };
-jest.setTimeout(50000);
+import { waitForAppScreen, zemu } from './test.fixture';
 
-test('Compare fixed rate screenshot', async () => {
-    const sim = new Zemu(APP_PATH, XRP_LIB);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz4");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("XRP");
-        tr.setCurrencyTo("XRP");
-        // 1 XRP to 10 XRP
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+test('[Nano S] Compare fixed rate screenshot', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz4");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("XRP");
+    tr.setCurrencyTo("XRP");
+    // 1 XRP to 10 XRP
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
-        await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
+    await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
 
-        const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
-        const checkRequest = swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters);
-        // Wait until we are not in the main menu
-        await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
+    const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
+    const checkRequest = swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters);
 
-        // Capture screenshots
-        await sim.clickRight("snapshots/tmp/send.png"); // Send screen
-        const send = Zemu.LoadPng2RGB("snapshots/tmp/send.png");
-        await sim.clickRight("snapshots/tmp/get.png"); // Get screen
-        const get = Zemu.LoadPng2RGB("snapshots/tmp/get.png");
-        await sim.clickRight("snapshots/tmp/fees.png"); // Fees screen
-        const fees = Zemu.LoadPng2RGB("snapshots/tmp/fees.png");
-        await sim.clickRight("snapshots/tmp/accept.png"); // Accept screen
-        const accept = Zemu.LoadPng2RGB("snapshots/tmp/accept.png");
-        await sim.clickBoth();
+    // Wait until we are not in the main menu
+    await waitForAppScreen(sim);
+    await sim.navigateAndCompareSnapshots('.', 'nanos_compare_fixed_rate', [4, 0]);
+    await expect(checkRequest).resolves.toBe(undefined);
+}));
 
-        // Load the expected results of the screenshots
-        const expected_send = Zemu.LoadPng2RGB("snapshots/fixed_rate/send.png");
-        const expected_get = Zemu.LoadPng2RGB("snapshots/fixed_rate/get.png");
-        const expected_fees = Zemu.LoadPng2RGB("snapshots/fixed_rate/fees.png");
-        const expected_accept = Zemu.LoadPng2RGB("snapshots/fixed_rate/accept.png");
 
-        // Compare expected results with actual data
-        expect(expected_send).toEqual(send);
-        expect(expected_get).toEqual(get);
-        expect(expected_fees).toEqual(fees);
-        expect(expected_accept).toEqual(accept);
+test('[Nano S] Compare floating rate screenshot', zemu("nanos", async (sim) => {
+    const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP, TRANSACTION_RATES.FLOATING);
+    const transactionId: string = await swap.startNewTransaction();
+    await swap.setPartnerKey(partnerSerializedNameAndPubKey);
+    await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
+    var tr = new proto.ledger_swap.NewTransactionResponse();
+    tr.setPayinAddress("2324234324324234");
+    tr.setPayinExtraId("");
+    tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz4");
+    tr.setRefundExtraId("");
+    tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
+    tr.setPayoutExtraId("");
+    tr.setCurrencyFrom("XRP");
+    tr.setCurrencyTo("XRP");
+    // 1 XRP to 10 XRP
+    tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
+    tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
+    tr.setDeviceTransactionId(transactionId);
 
-        await expect(checkRequest).resolves.toBe(undefined);
-    } finally {
-        await sim.close();
-    }
-})
+    const payload: Buffer = Buffer.from(tr.serializeBinary());
+    await swap.processTransaction(payload, 10000000);
+    const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
+    const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
+    await swap.checkTransactionSignature(signature);
+    const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
+    await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
 
-test('Compare floating rate screenshot', async () => {
-    const sim = new Zemu(APP_PATH, XRP_LIB);
-    try {
-        await sim.start(sim_options);
-        const swap = new Exchange(sim.getTransport(), TRANSACTION_TYPES.SWAP, TRANSACTION_RATES.FLOATING);
-        const transactionId: string = await swap.startNewTransaction();
-        await swap.setPartnerKey(partnerSerializedNameAndPubKey);
-        await swap.checkPartner(DERSignatureOfPartnerNameAndPublicKey);
-        var tr = new proto.ledger_swap.NewTransactionResponse();
-        tr.setPayinAddress("2324234324324234");
-        tr.setPayinExtraId("");
-        tr.setRefundAddress("rhBuYom8agWA4s7DFoM7AvsDA9XGkVCJz4");
-        tr.setRefundExtraId("");
-        tr.setPayoutAddress("ra7Zr8ddy9tB88RaXL8B87YkqhEJG2vkAJ");
-        tr.setPayoutExtraId("");
-        tr.setCurrencyFrom("XRP");
-        tr.setCurrencyTo("XRP");
-        // 1 XRP to 10 XRP
-        tr.setAmountToProvider(numberToBigEndianBuffer(100000000));
-        tr.setAmountToWallet(numberToBigEndianBuffer(1000000000));
-        tr.setDeviceTransactionId(transactionId);
+    const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
+    const checkRequest = swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters);
 
-        const payload: Buffer = Buffer.from(tr.serializeBinary());
-        await swap.processTransaction(payload, 10000000);
-        const digest: Buffer = Buffer.from(sha256.sha256.array(payload));
-        const signature: Buffer = secp256k1.signatureExport(secp256k1.sign(digest, swapTestPrivateKey).signature);
-        await swap.checkTransactionSignature(signature);
-        const xrpAddressParams = await getSerializedAddressParameters("44'/144'/0'/0/0");
-        await swap.checkPayoutAddress(XRPConfig, XRPConfigSignature, xrpAddressParams.addressParameters);
-
-        const xrp2AddressParams = await getSerializedAddressParameters("44'/144'/0'/1/0");
-        const checkRequest = swap.checkRefundAddress(XRPConfig, XRPConfigSignature, xrp2AddressParams.addressParameters);
-        // Wait until we are not in the main menu
-        await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
-
-        // Capture screenshots
-        await sim.clickRight("snapshots/tmp/send.png"); // Send screen
-        const send = Zemu.LoadPng2RGB("snapshots/tmp/send.png");
-        await sim.clickRight("snapshots/tmp/get.png"); // Get screen
-        const get = Zemu.LoadPng2RGB("snapshots/tmp/get.png");
-        await sim.clickRight("snapshots/tmp/fees.png"); // Fees screen
-        const fees = Zemu.LoadPng2RGB("snapshots/tmp/fees.png");
-        await sim.clickRight("snapshots/tmp/accept.png"); // Accept screen
-        const accept = Zemu.LoadPng2RGB("snapshots/tmp/accept.png");
-        await sim.clickBoth();
-
-        // Load the expected results of the screenshots
-        const expected_send = Zemu.LoadPng2RGB("snapshots/floating_rate/send.png");
-        const expected_get = Zemu.LoadPng2RGB("snapshots/floating_rate/get.png");
-        const expected_fees = Zemu.LoadPng2RGB("snapshots/floating_rate/fees.png");
-        const expected_accept = Zemu.LoadPng2RGB("snapshots/floating_rate/accept.png");
-
-        // Compare expected results with actual data
-        expect(expected_send).toEqual(send);
-        expect(expected_get).toEqual(get);
-        expect(expected_fees).toEqual(fees);
-        expect(expected_accept).toEqual(accept);
-
-        await expect(checkRequest).resolves.toBe(undefined);
-    } finally {
-        await sim.close();
-    }
-})
+    // Wait until we are not in the main menu
+    await waitForAppScreen(sim);
+    await sim.navigateAndCompareSnapshots('.', 'nanos_compare_floating_rate', [4, 0]);
+    await expect(checkRequest).resolves.toBe(undefined);
+}));
