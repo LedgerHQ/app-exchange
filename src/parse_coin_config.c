7d699dd2229eda2bd133fa75cfb2418a543ed350
Lucas PASCAL <lucas.pascal@ledger.com>
2022-04-27 15:12:21 +0200

    [fix] Issues highlighted by clang static analyzer

--- src/parse_coin_config.c
+++ src/parse_coin_config.c
@@ -1,6 +1,7 @@
+#include <string.h>
+#include <os.h>
+
 #include "parse_coin_config.h"
-#include "string.h"
-#include "os.h"
 
 typedef struct app_name_alias_s {
     char *foreign_name;
@@ -13,11 +14,10 @@ const app_name_alias_t appnames_aliases[] = {
 
 void set_ledger_application_name(buf_t *application_name) {
     for (size_t i = 0; i < sizeof(appnames_aliases) / sizeof(appnames_aliases[0]); i++) {
-        if (!strcmp((const char *) application_name->bytes,
-                    (char *) (PIC(appnames_aliases[i].foreign_name)))) {
-            // Copy the correct application name.
-            strcpy((char *) application_name->bytes, (char *) (PIC(appnames_aliases[i].app_name)));
-            // Update the application name size accordingly.
+        if (!strncmp((const char *) application_name->bytes,
+                     (char *) (PIC(appnames_aliases[i].foreign_name)),
+                     strlen((char *) PIC(appnames_aliases[i].foreign_name)))) {
+            application_name->bytes = (unsigned char *) appnames_aliases[i].app_name;
             application_name->size = strlen((char *) PIC(appnames_aliases[i].app_name));
             return;
         }
@@ -41,13 +41,18 @@ int parse_coin_config(buf_t *config_, buf_t *ticker, buf_t *application_name, bu
     application_name->size = 0;
     pure_config->bytes = 0;
     pure_config->size = 0;
+    // ticker
     if (config.size < 3) return 0;
     ticker->size = config.bytes[0];
     if (config.size < 3 + ticker->size) return 0;
     if (ticker->size > 0) ticker->bytes = config.bytes + 1;
+    // application_name
     application_name->size = config.bytes[1 + ticker->size];
-    if (config.size < 3 + ticker->size + application_name->size) return 0;
-    if (application_name->size > 0) application_name->bytes = config.bytes + 1 + ticker->size + 1;
+    if (config.size < 3 + ticker->size + application_name->size || application_name->size == 0) {
+        return 0;
+    }
+    application_name->bytes = config.bytes + 1 + ticker->size + 1;
+    // pure_config
     pure_config->size = config.bytes[1 + ticker->size + 1 + application_name->size];
     if (config.size != 3 + ticker->size + application_name->size + pure_config->size) return 0;
     if (pure_config->size > 0)
