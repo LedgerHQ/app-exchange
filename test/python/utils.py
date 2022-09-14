662a3a1f4216c6ff22aa9ff721ce15afce2540c1
Alexis Grojean <alexis.grojean@ledger.fr>
2022-09-14 15:34:38 +0200

    Add wait mechanism between check addr command and click events in python tests

--- test/python/utils.py
+++ test/python/utils.py
@@ -13,3 +13,17 @@ def int_to_bytes(x : int):
     if len(hexstring)%2: 
         hexstring = '0' + hexstring
     return bytes.fromhex(hexstring)
+
+def save_screenshot(client, path: str):
+    screenshot = client.get_screenshot()
+    img = Image.open(BytesIO(screenshot))
+    img.save(path)
+    
+def compare_screenshot_with_timeout(client, path: str, timeout_s = 5):
+    step = 0.5
+    for _ in range(int(timeout_s / step)):
+        screenshot = client.get_screenshot()
+        if speculos.client.screenshot_equal(path, BytesIO(screenshot)) :
+            return True
+        sleep(step)
+    return False
