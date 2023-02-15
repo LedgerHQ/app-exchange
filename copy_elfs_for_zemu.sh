b17325af0b37ecac34c15d4cf8837560262c7ada
Francois Beutin <francois.beutin@ledger.fr>
2023-02-15 13:03:52 +0100

    Use separated build system

---
+++ copy_elfs_for_zemu.sh
@@ -0,0 +1,30 @@
+mkdir test/elfs
+
+cp test/libs/bitcoin_legacy/nanos/bin/app.elf test/elfs/bitcoin_legacy_nanos.elf
+cp test/libs/bitcoin_legacy/nanos2/bin/app.elf test/elfs/bitcoin_legacy_nanosp.elf
+cp test/libs/bitcoin_legacy/nanox/bin/app.elf test/elfs/bitcoin_legacy_nanox.elf
+cp test/libs/bitcoin/nanos2/bin/app.elf test/elfs/bitcoin_nanosp.elf
+cp test/libs/bitcoin/nanox/bin/app.elf test/elfs/bitcoin_nanox.elf
+cp test/libs/bitcoin/nanos/bin/app.elf test/elfs/bitcoin_nanos.elf
+cp test/libs/ethereum_classic/nanos/bin/app.elf test/elfs/ethereum_classic_nanos.elf
+cp test/libs/ethereum_classic/nanos2/bin/app.elf test/elfs/ethereum_classic_nanosp.elf
+cp test/libs/ethereum_classic/nanox/bin/app.elf test/elfs/ethereum_classic_nanox.elf
+cp test/libs/ethereum/nanos/bin/app.elf test/elfs/ethereum_nanos.elf
+cp test/libs/ethereum/nanos2/bin/app.elf test/elfs/ethereum_nanosp.elf
+cp test/libs/ethereum/nanox/bin/app.elf test/elfs/ethereum_nanox.elf
+cp test/libs/litecoin/nanos/bin/app.elf test/elfs/litecoin_nanos.elf
+cp test/libs/litecoin/nanos2/bin/app.elf test/elfs/litecoin_nanosp.elf
+cp test/libs/litecoin/nanox/bin/app.elf test/elfs/litecoin_nanox.elf
+cp test/libs/stellar/nanos/bin/app.elf test/elfs/stellar_nanos.elf
+cp test/libs/stellar/nanos2/bin/app.elf test/elfs/stellar_nanosp.elf
+cp test/libs/stellar/nanox/bin/app.elf test/elfs/stellar_nanox.elf
+cp test/libs/tezos/nanos/bin/app.elf test/elfs/tezos_nanos.elf
+cp test/libs/tezos/nanos2/bin/app.elf test/elfs/tezos_nanosp.elf
+cp test/libs/tezos/nanox/bin/app.elf test/elfs/tezos_nanox.elf
+cp test/libs/xrp/nanos/bin/app.elf test/elfs/xrp_nanos.elf
+cp test/libs/xrp/nanos2/bin/app.elf test/elfs/xrp_nanosp.elf
+cp test/libs/xrp/nanox/bin/app.elf test/elfs/xrp_nanox.elf
+
+cp build/nanos/bin/app.elf test/elfs/exchange_nanos.elf
+cp build/nanos2/bin/app.elf test/elfs/exchange_nanosp.elf
+cp build/nanox/bin/app.elf test/elfs/exchange_nanox.elf
--- test/python/conftest.py
+++ test/python/conftest.py
@@ -3,13 +3,7 @@ from pathlib import Path
 from ragger.conftest import configuration
 
 
-# Change this value to the name of your application
-configuration.REQUIRED_CONFIGURATION["APP_NAME"] = "exchange"
-
-# Change this path to the path in which your compiled elf files are located
-configuration.REQUIRED_CONFIGURATION["APPS_DIRECTORY"] = (Path(__file__).parent.parent / "elfs").resolve()
-
-configuration.OPTIONNAL_CONFIGURATION["SIDELOADED_APPS"] = {
+configuration.OPTIONAL_CONFIGURATION["SIDELOADED_APPS"] = {
     "bitcoin": "Bitcoin",
     "bitcoin_legacy": "Bitcoin Legacy",
     "ethereum": "Ethereum",
@@ -18,8 +12,9 @@ configuration.OPTIONNAL_CONFIGURATION["SIDELOADED_APPS"] = {
     "xrp": "RXP",
     "litecoin": "Litecoin",
     "stellar": "Stellar",
-    "solana": "Solana",
 }
+
+configuration.OPTIONAL_CONFIGURATION["SIDELOADED_APPS_DIR"] = "test/libs"
 
 configuration.OPTIONAL_CONFIGURATION["BACKEND_SCOPE"] = "function"
 
