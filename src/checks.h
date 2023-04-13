2c8fc4d3afb67d53b7c8ae37c1f0c759a9fa80cd
Xavier Chapron <xavier.chapron@ledger.fr>
2023-04-13 13:46:27 +0200

    src: checks.h: Fix missing static qualifier

--- src/checks.h
+++ src/checks.h
@@ -4,7 +4,7 @@
 
 #include "globals.h"
 
-inline bool check_der_signature_length(const buf_t* const der) {
+static inline bool check_der_signature_length(const buf_t* const der) {
     if (der->size < MIN_DER_SIGNATURE_LENGTH || der->size > MAX_DER_SIGNATURE_LENGTH) {
         PRINTF("Error: DER signature length %d not in [%d, %d]\n",
                der->size,
@@ -15,7 +15,7 @@ inline bool check_der_signature_length(const buf_t* const der) {
     return true;
 }
 
-inline bool check_ticker_length(const buf_t* const ticker) {
+static inline bool check_ticker_length(const buf_t* const ticker) {
     if (ticker->size < TICKER_MIN_SIZE_B || ticker->size > TICKER_MAX_SIZE_B) {
         PRINTF("Error: Ticker length %d not in [%d, %d]\n",
                ticker->size,
@@ -26,7 +26,7 @@ inline bool check_ticker_length(const buf_t* const ticker) {
     return true;
 }
 
-inline bool check_app_name_length(const buf_t* const app_name) {
+static inline bool check_app_name_length(const buf_t* const app_name) {
     if (app_name->size < APPNAME_MIN_SIZE_B || app_name->size > BOLOS_APPNAME_MAX_SIZE_B) {
         PRINTF("Error: Application name length %d not in [%d, %d]\n",
                app_name->size,
