c0fc1c4499add060fdeb4e6b32459a283e8fac18
Francois Beutin <francois.beutin@ledger.fr>
2023-06-06 15:20:40 +0200

    Normalize ticker before comparison

--- src/process_transaction.h
+++ src/process_transaction.h
@@ -2,4 +2,8 @@
 
 #include "commands.h"
 
+void to_uppercase(char *str, unsigned char size);
+
+void set_ledger_currency_name(char *currency, size_t currency_size);
+
 int process_transaction(const command_t *cmd);
