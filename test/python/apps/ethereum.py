d09235f3b0cf153dccdbb3ae3c0e582533369fba
Francois Beutin <francois.beutin@ledger.fr>
2024-02-05 17:10:07 +0100

    wip

--- test/python/apps/ethereum.py
+++ test/python/apps/ethereum.py
@@ -7,7 +7,10 @@ ETH_CONF = create_currency_config("ETH", "Ethereum", ("ETH", 18))
 ETH_PACKED_DERIVATION_PATH = pack_derivation_path(ETH_PATH)
 
 
-BSC_CONF = create_currency_config("BNB", "Binance Smart Chain", ("BNB", 18))
+# BSC_CONF = create_currency_config("BNB", "Binance Smart Chain", ("BNB", 18))
+# BSC_PACKED_DERIVATION_PATH = pack_derivation_path(ETH_PATH)
+
+BSC_CONF = create_currency_config("BNB", "Ethereum", ("BNB", 18))
 BSC_PACKED_DERIVATION_PATH = pack_derivation_path(ETH_PATH)
 
 ETC_PATH = "m/44'/60'/0'/0/0"
