d3a75b8dae0cca25b8884a1ab035a2df166d772f
Francois Beutin <francois.beutin@ledger.fr>
2024-04-25 11:57:33 +0200

    Factorize ETH and BSC test runner class

--- test/python/test_ethereum.py
+++ test/python/test_ethereum.py
@@ -6,9 +6,8 @@ from ledger_app_clients.ethereum.client import EthAppClient
 from .apps import cal as cal
 
 
-# ExchangeTestRunner implementation for Ethereum
-class EthereumTests(ExchangeTestRunner):
-    currency_configuration = cal.ETH_CURRENCY_CONFIGURATION
+# ExchangeTestRunner implementation for all Ethereum network
+class GenericEthereumNetworkTests(ExchangeTestRunner):
     valid_destination_1 = "0xd692Cb1346262F584D17B4B470954501f6715a82"
     valid_destination_memo_1 = ""
     valid_destination_2 = "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E"
@@ -36,14 +35,40 @@ class EthereumTests(ExchangeTestRunner):
                                  "gas": fees,
                                  "to": destination,
                                  "value": send_amount,
-                                 "chainId": 1
+                                 "chainId": self.chain_id
                              }):
             pass
         # TODO : assert signature validity
 
-# Use a class to reuse the same Speculos instance
-class TestsEthereum:
 
+# ExchangeTestRunner implementation for native ETH
+class EthereumTests(GenericEthereumNetworkTests):
+    chain_id = 1
+    currency_configuration = cal.ETH_CURRENCY_CONFIGURATION
+
+class TestsEthereum:
     @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO)
     def test_ethereum(self, backend, exchange_navigation_helper, test_to_run):
         EthereumTests(backend, exchange_navigation_helper).run_test(test_to_run)
+
+
+# ExchangeTestRunner implementation for BSC on ETH application
+class BSCTests(GenericEthereumNetworkTests):
+    chain_id = 56
+    currency_configuration = cal.BNB_CURRENCY_CONFIGURATION
+
+class TestsBSC:
+    @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO)
+    def test_bsc(self, backend, exchange_navigation_helper, test_to_run):
+        BSCTests(backend, exchange_navigation_helper).run_test(test_to_run)
+
+
+# ExchangeTestRunner implementation for BSC on BNB application
+class BSCLegacyTests(GenericEthereumNetworkTests):
+    chain_id = 56
+    currency_configuration = cal.BNB_LEGACY_CURRENCY_CONFIGURATION
+
+class TestsBSCLegacy:
+    @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO)
+    def test_bsc_legacy(self, backend, exchange_navigation_helper, test_to_run):
+        BSCLegacyTests(backend, exchange_navigation_helper).run_test(test_to_run)
