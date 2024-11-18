96d125e70553ccaa1cfd9cda7092e71b3b9a4708
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-11-18 17:49:34 +0100

    Add cardano tests based on Shelley addresses

--- test/python/apps/cardano.py
+++ test/python/apps/cardano.py
@@ -84,3 +84,58 @@ class CardanoClient:
 
         self._check_signature_validity(path, signature, data)
 
+
+    def perform_shelley_sign_tx(self, destination: str, send_amount: int, send_fees: int) -> None:
+        """Send a Sign TX command to the Cardano app.
+           Based on Shelley, address type DEVICE_OWNED
+
+        Args:
+            destination (str): Destination address
+            send_amount (int): Amount to send
+            send_fees (int): Fees to pay
+
+        Returns:
+            Tuple[bytes, bytes]: Data Hash and the Signature
+        """
+
+        if destination is None:
+            raise ValueError(f"Invalid destination address: {destination}")
+
+        witness_path1 = "m/1852'/1815'/2'/1/0"
+        witness_path2 = "m/1852'/1815'/2'/2/0"
+        dest_len = len(destination) // 2
+        output_len = 6 + dest_len + len(send_amount) // 2 + 6
+        signTx = {
+            # Based on LL example output
+            "getVersion": "d700000000",
+            "deriveAddress": "d71101002d0001058000073c8000071780000002000000000000000222058000073c80000717800000020000000200000000",
+            "signTxInit": "d72101003c0000000000000000012d964a090201010101010101010103000000010000000200000000000000010000000000000000000000000000000000000002",
+            "signTxInputs": "d7210200247c50edd7d2b7891dc3c33fade1ea557ac748bf177af7088e622f1454b6f71d0d00000000",
+            # Send To address (destination)
+            "signTxOutputBasic1": f"d7210330{output_len:02x}0101{dest_len:08x}{destination}{send_amount}000000000101",
+            "signTxOutputConfirm1": "d721033300",
+            # Send From address
+            "signTxOutputBasic2": "d72103303d01020001058000073c8000071780000002000000010000000122058000073c800007178000000200000002000000000000000000476009000000000101",
+            "signTxOutputConfirm2": "d721033300",
+            "signTxFees": f"d721040008{send_fees}",
+            "signTxTtl": "d72105000800000000085f492b",
+            "SignTxWithdrawals": "d72107001e0000000000004f1b00058000073c80000717800000020000000200000000",
+            "signTxConfirm": "d7210a0000",
+            "signTxWitness1": f"d7210f0015{pack_derivation_path(witness_path1).hex()}",
+            "signTxWitness2": f"d7210f0015{pack_derivation_path(witness_path2).hex()}",
+        }
+
+        data = bytes()
+        for step, value in signTx.items():
+            response = self._backend.exchange_raw(bytes.fromhex(value))
+            assert response.status == Errors.SW_SUCCESS
+            if step == "signTxConfirm":
+                # Retrieve the data hash
+                data = response.data
+            elif step.startswith("signTxWitness"):
+                # Retrieve the signature
+                signature = response.data
+                # Verify the signature for the selected path
+                witness_path = witness_path1 if step == "signTxWitness1" else witness_path2
+                self._check_signature_validity(witness_path, signature, data)
+
--- test/python/test_cardano.py
+++ test/python/test_cardano.py
@@ -1,9 +1,14 @@
 import pytest
 import base58
 
+from ragger.bip import pack_derivation_path
+from ragger.backend import BackendInterface
+from ragger.firmware import Firmware
+
 from .apps.exchange_test_runner import ExchangeTestRunner, ALL_TESTS_EXCEPT_MEMO_AND_THORSWAP
+from .apps.exchange_navigation_helper import ExchangeNavigationHelper
 from .apps.cardano import CardanoClient, Errors
-from .apps.cardano import ADA_BYRON_DERIVATION_PATH
+from .apps.cardano import ADA_BYRON_DERIVATION_PATH, ADA_SHELLEY_DERIVATION_PATH
 from .apps import cal as cal
 
 
@@ -30,9 +35,58 @@ class CardanoByronClientTests(ExchangeTestRunner):
                                                           f"{fees:016x}")
 
 
+SHELLEY_DESTINATION = {
+    "valid_1": {
+        "addr": "addr1q80r70qggedqy90z4rzy6kynv4xqejxfxqmangwhz8ugalfwlqyt4mswmh4hl0nnq53r4rp798vj4c7p7f2wdgqnc8uqt2xltv",
+        "path": "01de3f3c08465a0215e2a8c44d5893654c0cc8c93037d9a1d711f88efd2ef808baee0eddeb7fbe7305223a8c3e29d92ae3c1f254e6a013c1f8"},
+    "valid_2": {
+        "addr": "addr1q84sh2j72ux0l03fxndjnhctdg7hcppsaejafsa84vh7lwgmcs5wgus8qt4atk45lvt4xfxpjtwfhdmvchdf2m3u3hlsd5tq5r",
+        "path": "01eb0baa5e570cffbe2934db29df0b6a3d7c0430ee65d4c3a7ab2fefb91bc428e4720702ebd5dab4fb175324c192dc9bb76cc5da956e3c8dff"},
+}
+
+# ExchangeTestRunner implementation for Cardano
+class CardanoShelleyClientTests(ExchangeTestRunner):
+
+    # Addresses Generated on MainNet, type BASE_PAYMENT_KEY_STAKE_KEY
+    currency_configuration = cal.ADA_SHELLEY_CURRENCY_CONFIGURATION
+    valid_destination_1 = SHELLEY_DESTINATION["valid_1"]["addr"]
+    valid_destination_2 = SHELLEY_DESTINATION["valid_2"]["addr"]
+    # Refund adress corresponds to the derivation path "m/1852'/1815'/0'/0/0" and "m/1852'/1815'/0'/2/0"
+    valid_refund = "addr1q9kl5z2zd9vakyprvw0g68c8hv0y0rnj93htc82hh2rs8wwmyx0wtn56wnuclkku9hsnal8dtg25a7x56svjn4dlnlmq7quz6p"
+    valid_send_amount_1 = 4671693
+    valid_send_amount_2 = 446739662
+    valid_fees_1 = 174345
+    valid_fees_2 = 28
+    fake_refund = "abcdabcd"
+    fake_payout = "abcdabcd"
+    signature_refusal_error_code = Errors.SW_SWAP_CHECKING_FAIL
+
+    def perform_final_tx(self, destination: str, send_amount: int, fees: int, memo) -> None:
+        # Create a reverse lookup dictionary
+        addr_to_path = {v["addr"]: v["path"] for v in SHELLEY_DESTINATION.values()}
+        CardanoClient(self.backend).perform_shelley_sign_tx(addr_to_path.get(destination),
+                                                            f"{send_amount:016x}",
+                                                            f"{fees:016x}")
+
+
 # Use a class to reuse the same Speculos instance
 class TestsCardanoClient:
 
+    # Test Ready, but legacy and unused address format
+    # @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO_AND_THORSWAP)
+    # def test_cardano_byron(self, 
+    #                        backend: BackendInterface,
+    #                        exchange_navigation_helper: ExchangeNavigationHelper,
+    #                        test_to_run: str) -> None:
+    #     if backend.firmware == Firmware.NANOS:
+    #         pytest.skip("Polkadot swap is not supported on NanoS device")
+    #     CardanoByronClientTests(backend, exchange_navigation_helper).run_test(test_to_run)
+
     @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO_AND_THORSWAP)
-    def test_cardano(self, backend, exchange_navigation_helper, test_to_run):
-        CardanoByronClientTests(backend, exchange_navigation_helper).run_test(test_to_run)
+    def test_cardano_shelley(self,
+                             backend: BackendInterface,
+                             exchange_navigation_helper: ExchangeNavigationHelper,
+                             test_to_run: str) -> None:
+        if backend.firmware == Firmware.NANOS:
+            pytest.skip("Polkadot swap is not supported on NanoS device")
+        CardanoShelleyClientTests(backend, exchange_navigation_helper).run_test(test_to_run)
