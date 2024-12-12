68935ad2d3fea42a0afc118fe094cde16161bcdb
GroM <jerome.caporossi@ledger.fr>
2024-12-12 16:30:42 +0100

    Update Near tests

--- test/python/apps/near.py
+++ test/python/apps/near.py
@@ -2,22 +2,9 @@ from enum import IntEnum
 
 from ragger.bip import pack_derivation_path
 from ragger.utils import create_currency_config, RAPDU
-from py_near.transactions import create_transfer_action, Transaction
-import ed25519
-import base58
-
-
-ED25519_KEYPAIR = "188d2ce61071d477a2400558c3612ee68957a80aa2e56c29dc4da2dace58e7d8c4f5941e81e071c2fd1dae2e71fd3d859d462484391d9a90bf219211dcbb320f"
-PRIVATE_KEY = ed25519.SigningKey(bytes.fromhex(ED25519_KEYPAIR))
-PUBLIC_KEY = PRIVATE_KEY.get_verifying_key()
 
 NEAR_CONF = create_currency_config("NEAR", "NEAR")
-
 NEAR_PACKED_DERIVATION_PATH = pack_derivation_path("m/44'/397'/0'/0'/1'")
-SIGNER_ID = "blablatest.testnet"
-NONCE = 96520360000015
-BLOCK_HASH = base58.b58decode("C32rfeBkSMT1xnsrArkV9Mu81ww9qK7n6Kw17NhEbVuK")
-
 
 class Ins():
     SIGN = 0x02
@@ -46,15 +33,24 @@ class NearClient:
     def send_simple_sign_tx(self, path: str, destination: str, send_amount: int) -> RAPDU:
         packed_path = pack_derivation_path(path)
 
-        tx = Transaction(
-                signer_id="blablatest.testnet",
-                public_key=PUBLIC_KEY.to_bytes(),
-                nonce=NONCE,
-                receiver_id=destination,
-                actions=[create_transfer_action(send_amount * 10**24)],
-                block_hash=BLOCK_HASH
-                )
+        """
+        1234.56 NEAR
+        Transaction {
+            signer_id: "blablatest.testnet",
+            public_key: ed25519:EFr6nRvgKKeteKoEH7hudt8UHYiu94Liq2yMM7x2AU9U,
+            nonce: 103595482000005,
+            receiver_id: "speculos.testnet",
+            block_hash: Cb3vKNiF3MUuVoqfjuEFCgSNPT79pbuVfXXd2RxDXc5E,
+            actions: [
+                Transfer(
+                    TransferAction {
+                        deposit: 1234560000000000000000000000,
+                    },
+                ),
+            ],
+        }
+        """
 
-        serialized_tx = bytes(tx.to_vec(PRIVATE_KEY.to_bytes()))
+        tx = bytes.fromhex("12000000626c61626c61746573742e746573746e657400c4f5941e81e071c2fd1dae2e71fd3d859d462484391d9a90bf219211dcbb320f85aae733385e00001000000073706563756c6f732e746573746e6574ac299ac1376e375cd39338d8b29225613ef947424b74a3207c1226863a72583101000000030000001049f203b43f34fd0300000000")
 
-        return self._backend.exchange(self.CLA, Ins.SIGN, P1.MORE, P2.UNUSED, packed_path[1:] + serialized_tx)
+        return self._backend.exchange(self.CLA, Ins.SIGN, P1.MORE, P2.UNUSED, packed_path[1:] + tx)
