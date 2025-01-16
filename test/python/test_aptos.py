5b6b645f0bcec482568826cbaec46f8307c4f6f9
João Martins <joaomartinsfcup@gmail.com>
2025-01-16 18:18:23 +0000

    Fixed config on Aptos test.

--- test/python/test_aptos.py
+++ test/python/test_aptos.py
@@ -6,6 +6,21 @@ from .apps import cal as cal
 
 # ExchangeTestRunner implementation for Ton
 class AptosTests(ExchangeTestRunner):
+    currency_configuration = cal.APTOS_CURRENCY_CONFIGURATION
+    valid_destination_1 = "TNbtZSpknaQvC7jPCLU4znJMgm8fhuGTTY"
+    valid_destination_memo_1 = ""
+    valid_destination_2 = "TBoTZcARzWVgnNuB9SyE3S5g1RwsXoQL16"
+    valid_destination_memo_2 = ""
+    valid_refund = "TNbtZSpknaQvC7jPCLU4znJMgm8fhuGTTY"
+    valid_refund_memo = ""
+    valid_send_amount_1 = 1000000
+    valid_send_amount_2 = 446739662
+    valid_fees_1 = 0
+    valid_fees_2 = 1
+    fake_refund = "abcdabcd"
+    fake_refund_memo = "1"
+    fake_payout = "abcdabcd"
+    fake_payout_memo = "1"
 
     def perform_final_tx(self, destination, send_amount, fees, memo):
         assert False
