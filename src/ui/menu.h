7e29b58f211aa012ebe919d259308fa8be22c269
GroM <jerome.caporossi@ledger.fr>
2025-02-25 16:24:49 +0100

    Fix CodeQL CI

--- src/ui/menu.h
+++ src/ui/menu.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #if defined(SCREEN_SIZE_WALLET)
-#define ICON_APP_WARNING &C_Warning_64px
-#define ICON_APP_EXCHANGE &C_icon_exchange_64x64
+#define ICON_APP_WARNING  &C_Warning_64px
+#define ICON_APP_EXCHANGE  &C_icon_exchange_64x64
 #ifndef TEST_BUILD
 #define HOME_TEXT "This app enables swapping\nand selling assets\nin Ledger Live."
 #else
@@ -10,7 +10,7 @@
 #endif
 #else  // SCREEN_SIZE_WALLET
 #define ICON_APP_EXCHANGE NULL
-#define ICON_APP_WARNING &C_icon_warning
+#define ICON_APP_WARNING  &C_icon_warning
 #ifndef TEST_BUILD
 #define HOME_TEXT "Exchange is ready"
 #else
