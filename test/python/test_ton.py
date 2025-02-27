8574521a1f183f3bb5f5d2623b8cf32ab3555899
Florent Valette <florent.valette@ledger.fr>
2025-02-27 17:30:37 +0100

    test: enable exchange ton usdt test

--- test/python/test_ton.py
+++ test/python/test_ton.py
@@ -66,6 +66,49 @@ class TonTests(ExchangeTestRunner):
         tx_bytes = tx.to_request_bytes()
 
         # Send the sign device instruction.
+        # As it requires on-screen validation, the function is asynchronous.
+        # It will yield the result when the navigation is done
+        with client.sign_tx(path=TON_DERIVATION_PATH, transaction=tx_bytes):
+            pass
+
+        # The device as yielded the result, parse it and ensure that the signature is correct
+        response = client.get_async_response().data
+        sig, hash_b = unpack_sign_tx_response(response)
+        assert hash_b == tx.transfer_cell().bytes_hash()
+        assert check_signature_validity(pubkey, sig, hash_b)
+
+class TonUSDTTests(TonTests):
+    currency_configuration = cal.TON_USDT_CURRENCY_CONFIGURATION
+    valid_destination_1 = "UQDWey_FGPhd3phmerdVXi-zUIujfyO4-29y_VT1yD0meY1n"
+    valid_destination_2 = "EQANxfGN1EgFPawYB1fhPqebKe1Nb6FIsaiekEecJ6R-3kYF"
+    # user jetton wallet address
+    valid_refund = "EQD0sKn8DbS12U015TWOSpYmyJYYDC_7sxg1upaMxnBvTiX8"
+    fake_refund = "UQDWey_FGPhd3phmerdVXi-zUIujfyO4-29y_VT1yD0meY1n"
+    fake_payout = "UQDWey_FGPhd3phmerdVXi-zUIujfyO4-29y_VT1yD0meY1n"
+
+    def perform_final_tx(self, destination, send_amount, fees, memo):
+        # Use the app interface instead of raw interface
+        client = BoilerplateCommandSender(self.backend)
+        dest_address = Address(destination)
+
+        # First we need to get the public key of the device in order to build the transaction
+        pubkey = client.get_public_key(path=TON_DERIVATION_PATH).data
+
+        payload = JettonTransferPayload(fees, dest_address, jetton_id=0, forward_amount=1)
+
+        # transaction for Jetton transfer are sent to the owned jetton wallet
+        # destination address is serialized in Jetton Transfer Payload
+        tx = Transaction(
+            Address(self.valid_refund),
+            SendMode.PAY_GAS_SEPARATLY,
+            seqno=0,
+            timeout=1686176000,
+            bounce=dest_address.is_bounceable,
+            amount=send_amount,
+            payload=payload,
+        )
+        tx_bytes = tx.to_request_bytes()
+
         with client.sign_tx(path=TON_DERIVATION_PATH, transaction=tx_bytes):
             # As there is no display inside the TON call when using the application through
             # Exchange, we simply end the asynchronism.
@@ -85,36 +128,11 @@ class TestsTon:
     @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO_THORSWAP_AND_FEES)
     def test_ton(self, backend, exchange_navigation_helper, test_to_run):
         if backend.firmware.device == "nanos":
-            pytest.skip("TON swap is not supported on NanoS device")
-
-        # Forward to the ExchangeTestRunner the parametrized test to run
+            pytest.skip("Ton swap is not supported on NanoS device")
         TonTests(backend, exchange_navigation_helper).run_test(test_to_run)
-# --8<-- [end:native_test]
 
-# class TonUSDTTests(TonTests):
-#     currency_configuration = cal.TON_CURRENCY_CONFIGURATION
-#     def perform_final_tx(self, destination, send_amount, fees, memo):
-#         # Use the app interface instead of raw interface
-#         client = BoilerplateCommandSender(self.backend)
-
-#         # First we need to get the public key of the device in order to build the transaction
-#         pubkey = client.get_public_key(path=TON_DERIVATION_PATH).data
-
-#         payload = JettonTransferPayload(100, Address("0:" + "0" * 64), forward_amount=1)
-
-#         tx = Transaction(Address("0:" + "0" * 64), SendMode.PAY_GAS_SEPARATLY, 0, 1686176000, True, 100000000, payload=payload)
-#         tx_bytes = tx.to_request_bytes()
-
-#         with client.sign_tx(path=TON_DERIVATION_PATH, transaction=tx_bytes):
-#             pass
-
-#         # The device as yielded the result, parse it and ensure that the signature is correct
-#         response = client.get_async_response().data
-#         sig, hash_b = unpack_sign_tx_response(response)
-#         assert hash_b == tx.transfer_cell().bytes_hash()
-#         assert check_signature_validity(pubkey, sig, hash_b)
-
-# class TestsTonUSDT:
-#     @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO_THORSWAP_AND_FEES)
-#     def test_ton_usdt(self, backend, exchange_navigation_helper, test_to_run):
-#         TonUSDTTests(backend, exchange_navigation_helper).run_test(test_to_run)
+    @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO_THORSWAP_AND_FEES)
+    def test_ton_usdt(self, backend, exchange_navigation_helper, test_to_run):
+        if backend.firmware.device == "nanos":
+            pytest.skip("Ton swap is not supported on NanoS device")
+        TonUSDTTests(backend, exchange_navigation_helper).run_test(test_to_run)
