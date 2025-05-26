dc01e75dbadd1447bf23a50c663b50e70d4edb53
GroM <jerome.caporossi@ledger.fr>
2025-05-26 10:51:54 +0200

    Include PEM file in Python package

--- client/src/ledger_app_clients/exchange/pki/pem_signer.py
+++ client/src/ledger_app_clients/exchange/pki/pem_signer.py
@@ -1,3 +1,5 @@
+import importlib.resources
+
 import os
 import hashlib
 from ecdsa import SigningKey
@@ -9,14 +11,13 @@ class KeySigner:
         """
         Initialize the KeySigner instance with a specific PEM file.
 
-        :param pem_name: Name of the PEM file (without extension), e.g., 'trusted_name'
+        :param pem_name: Name of the PEM file (e.g., 'trusted_name.pem')
         """
-        pem_path = os.path.join(os.path.dirname(__file__), f"{pem_name}")
-        if not os.path.exists(pem_path):
-            raise FileNotFoundError(f"PEM file not found: {pem_path}")
-
-        with open(pem_path, "r") as pem_file:
-            self._signing_key = SigningKey.from_pem(pem_file.read(), hashlib.sha256)
+        try:
+            with importlib.resources.open_text("ledger_app_clients.exchange.pki", pem_name) as pem_file:
+                self._signing_key = SigningKey.from_pem(pem_file.read(), hashlib.sha256)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"PEM file not found in package: {pem_name}")
 
     def sign_data(self, data: bytes) -> bytes:
         """
